a3be24f75b7e23b4ec817212c86169421f84b1a1
HseinKt <hseinkteish1998@gmail.com>
2023-02-27 22:49:30 +0200

    Created canva and button letters using javascript.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+const hangman = document.getElementById("hangman")
+const hint = document.getElementById("hint")
+const guess = document.getElementById("guess")
+const characters = document.getElementById("characters")
+const play = document.getElementById("play")
+const alphabetArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] ;
+
+for (let i=0 ; i<alphabetArray.length ; i++) {
+    const btn = document.createElement('button')
+    btn.innerHTML = alphabetArray[i]
+    btn.className = "btn_char"
+    btn.id = "btn_char"
+    characters.appendChild(btn)
+}
+
+play.addEventListener('click', () => {
+    const c = hangman.getContext("2d")
+    c.fillStyle = "black";
+    c.fillRect(20, 20, 150, 100);
+})
